84eaecb7f81e66f324def470952b1c0661f419c3
Ji Chao <chaojifudan@outlook.com>
2016-12-03 21:09:17 +0800

    添加adapter

--- Client/Dada/app/src/main/java/projectmanager/dada/pages/MyAcceptTaskActivity.java
+++ Client/Dada/app/src/main/java/projectmanager/dada/pages/MyAcceptTaskActivity.java
@@ -2,14 +2,40 @@ package projectmanager.dada.pages;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.List;
 import projectmanager.dada.R;
+import projectmanager.dada.adapter.ViewMyAcceptTaskAdapter;
+import projectmanager.dada.model.Task;
 
 public class MyAcceptTaskActivity extends AppCompatActivity {
+
+    ListView myAcceptListView;
+    List<Task> myAcceptTaskList;
+    ViewMyAcceptTaskAdapter myAcceptTaskAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_accept_task);
+
+        buildData();
+
+        myAcceptListView = (ListView) findViewById(R.id.my_accept_task_list_view);
+        myAcceptTaskAdapter = new ViewMyAcceptTaskAdapter(MyAcceptTaskActivity.this,
+                R.layout.my_accept_task_view,myAcceptTaskList);
+        myAcceptListView.setAdapter(myAcceptTaskAdapter);
     }
+
+
+    private void buildData(){
+        myAcceptTaskList = new ArrayList<>();
+        Task task1 = new Task(0,"第一条","第一条的内容",null,null,null,null,null,0,0,null);
+        myAcceptTaskList.add(task1);
+        Task task2 = new Task(0,"第二条","第二条的内容",null,null,null,null,null,0,0,null);
+        myAcceptTaskList.add(task2);
+    }
+
+
 }
--- Client/Dada/app/src/main/java/projectmanager/dada/pages/MyPublishTaskActivity.java
+++ Client/Dada/app/src/main/java/projectmanager/dada/pages/MyPublishTaskActivity.java
@@ -2,13 +2,39 @@ package projectmanager.dada.pages;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import projectmanager.dada.R;
+import projectmanager.dada.adapter.ViewMyPublishTaskAdapter;
+import projectmanager.dada.model.Task;
 
 public class MyPublishTaskActivity extends AppCompatActivity {
+
+    ListView myPublishListView;
+    List<Task> myPublishTaskList;
+    ViewMyPublishTaskAdapter myPublishTaskAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_publish_task);
+
+        buildData();
+
+        myPublishListView = (ListView) findViewById(R.id.my_publish_task_list_view);
+        myPublishTaskAdapter = new ViewMyPublishTaskAdapter(MyPublishTaskActivity.this,
+                R.layout.my_publish_task_view,myPublishTaskList);
+        myPublishListView.setAdapter(myPublishTaskAdapter);
+    }
+
+    private void buildData(){
+        myPublishTaskList = new ArrayList<>();
+        Task task1 = new Task(0,"第一条","第一条的内容",null,null,null,null,null,0,0,null);
+        myPublishTaskList.add(task1);
+        Task task2 = new Task(0,"第二条","第二条的内容",null,null,null,null,null,0,0,null);
+        myPublishTaskList.add(task2);
     }
 }

17f29e85e9e543617813baa5943b39c139144a03
JScarlet <x18321758289@outlook.com>
2016-12-05 14:20:24 +0800

    添加地图
    添加了地图和jar包，把重复的http.legacy的包删掉了

--- Client/Dada/app/src/main/java/projectmanager/dada/pages/NearbyTaskActivity.java
+++ Client/Dada/app/src/main/java/projectmanager/dada/pages/NearbyTaskActivity.java
@@ -3,13 +3,44 @@ package projectmanager.dada.pages;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.amap.api.maps2d.MapView;
+
 import projectmanager.dada.R;
 
 public class NearbyTaskActivity extends AppCompatActivity {
 
+    MapView mapView = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nearby_task);
+
+        mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，实现地图生命周期管理
+        mapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，实现地图生命周期管理
+        mapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，实现地图生命周期管理
+        mapView.onSaveInstanceState(outState);
     }
 }

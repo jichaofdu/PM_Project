53e982910869a1069411425b5666e14ff13df1a0
W <lixia_lei@outlook.com>
2016-12-22 20:19:45 +0800

    修改了应用说明

--- Client/Dada/app/src/main/java/projectmanager/dada/pages/ProjectInstructionActivity.java
+++ Client/Dada/app/src/main/java/projectmanager/dada/pages/ProjectInstructionActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import projectmanager.dada.R;
 
 /**
@@ -30,10 +31,12 @@ public class ProjectInstructionActivity extends AppCompatActivity implements Vie
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         imgIdArray = new int[]{R.drawable.item1, R.drawable.item2, R.drawable.item3, R.drawable.item4};
         textDescription = (TextView) findViewById(R.id.viewPagerDescription);
-        description = new String[]{"1. 哒哒找人（后简称“本应用”）是一款使用移动互联网技术来为实际生活提供便利的应用，类似于“嘀嘀打车”。在生活中我们常常有很多需要其他人帮助的一些小事情，比如帮忙拿快递这样的很小的事情但是却一时找不到合适的人来帮忙。本应用的目的在于打破这种信息不对称的情况，可以让更多的人知道用户的求助，来帮用户完成任务。",
-                "2. “嘀嘀打车”通过地图和GPS定位系统，使得用户可以在自己指定的地点附近呼叫或预约出租车。该呼叫或预约请求将被广播给附近的出租车司机。收到通知的司机可以选择接受或不接受请求。但是“滴滴打车”的应用场景较窄，仅仅囊括了用户的出行打车需求。当用户需要其它类型的协助时，“滴滴打车”将不能胜任类似任务。",
-                "3. 与“滴滴打车”类似，本应用将应用场景从“预约出租车”上进行了一定的扩展。本应用允许用户将自己需要帮助的请求发送到指定地点附近的其他用户的手机上，然后获得附近比较空闲的用户的帮助来完成任务。求助类型可以包括“代领快递”、“借用物品”、“问卷填写”、“个性化内容推送”或者其它用户自定义的类型。",
-                "4. 本应用涉及的任务对于任务的接受者来说都是一些不需要用太多时间和精力都能完成的小任务，但这些任务对于任务的发出者却可以帮很大的忙。这种任务特性保证了任务的接收者不用因为任务将占用自己的太多时间而拒绝接受任务的情况出现。"};
+        description = new String[]{
+                "哒哒找人是一个基于信誉值的任务互助平台。您可以利用本应用在指定地点发布任务，轻松解决诸如“代领快递”、“借用物品”、“问卷填写”的生活问题~",
+                "投之以桃，报之以李~\n发布任务会消耗2点基础信誉值+自定义悬赏信誉值，接受任务会获得3点基础信誉值+任务悬赏信誉值奖励。",
+                "取消任务须谨慎~\n任务发布者需要在接受者完成任务后进行确认。若任务发布者在发布的任务未被接受时取消任务，不会受到信誉值惩罚，但若该任务已被人接受，则发布者会被扣除6点信誉值作为惩罚~",
+                "接受任务量力而行~\n若要中途放弃自己已接受的任务，将会被扣除5点信誉值。若自己已接受的任务超过deadline还未完成，则会被扣除6点信誉值~"
+        };
 
      /*   imageViews = new ImageView[imgIdArray.length];
         for(int i = 0; i < imageViews.length; i++){
@@ -57,12 +60,12 @@ public class ProjectInstructionActivity extends AppCompatActivity implements Vie
                     return true;
                 }
             });
-        }else if (imgIdArray.length == 2 || imgIdArray.length == 3) {
+        } else if (imgIdArray.length == 2 || imgIdArray.length == 3) {
             imageViews = new ImageView[imgIdArray.length * 2];
             for (int i = 0; i < (imageViews.length); i++) {
                 ImageView imageView = new ImageView(this);
                 imageViews[i] = imageView;
-                imageView.setBackgroundResource(imgIdArray[(i > (imgIdArray.length-1)) ? i -imgIdArray.length  : i]);
+                imageView.setBackgroundResource(imgIdArray[(i > (imgIdArray.length - 1)) ? i - imgIdArray.length : i]);
             }
         } else {
             imageViews = new ImageView[imgIdArray.length];
@@ -104,7 +107,7 @@ public class ProjectInstructionActivity extends AppCompatActivity implements Vie
         }
     }
 
-    public class ViewPagerAdapter extends PagerAdapter{
+    public class ViewPagerAdapter extends PagerAdapter {
 
         @Override
         public int getCount() {
@@ -118,12 +121,12 @@ public class ProjectInstructionActivity extends AppCompatActivity implements Vie
 
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
-            ((ViewPager)container).removeView(imageViews[position % imageViews.length]);
+            ((ViewPager) container).removeView(imageViews[position % imageViews.length]);
         }
 
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
-            ((ViewPager)container).addView(imageViews[position % imageViews.length], 0);
+            ((ViewPager) container).addView(imageViews[position % imageViews.length], 0);
             return imageViews[position % imageViews.length];
         }
     }
